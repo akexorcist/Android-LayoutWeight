6914e977ab1546da44e43ac9666426a8460d499e
akexorcist <akexorcist@gmail.com>
2014-06-13 03:01:40 +0700

    ...
    ...

---
+++ src/app/akexorcist/layoutweight/Main.java
@@ -0,0 +1,24 @@
+package app.akexorcist.layoutweight;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import app.akexorcist.layoutwidth.R;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
